63d4dddd59e014a4abd46db9b51941d01a928e30
akulamartin <martin.akula@gmail.com>
2020-09-18 01:53:44 +0300

    Minor edit.

--- app/src/main/java/com/example/leaderboard/RequestInterface.java
+++ app/src/main/java/com/example/leaderboard/RequestInterface.java
@@ -19,7 +19,6 @@ public interface RequestInterface {
     Call <List<GADSDevelopersIqSkillsModel>> getJSONSkillsIQ();
 
     @POST("1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/formResponse")
-
     @FormUrlEncoded
     Call<Void> sendDetails(
             @Field( "entry.1877115667")String FirstName,
